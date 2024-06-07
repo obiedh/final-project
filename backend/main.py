5240c6902d024c61e821a13ed01d236143f51209
obiedh <haddadebed@gmail.com>
2024-06-08 00:36:39 +0300

    Initial commit with project structure and virtual environment setup

---
+++ backend/main.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from svc.db import db
+from api import api_bp
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost/postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+bcrypt = Bcrypt(app)
+
+app.register_blueprint(api_bp, url_prefix='/api')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)#for deveolpminet env
+
+
+
+
